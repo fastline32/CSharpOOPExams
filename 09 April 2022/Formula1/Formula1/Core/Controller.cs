c16a464ba34dfaf032e92590762eeddfcce970e9
Svetoslav Savov <speednet@mail.bg>
2022-04-09 13:06:46 +0300

    new

---
+++ 09 April 2022/Formula1/Formula1/Core/Controller.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Formula1.Core.Contracts;
+using Formula1.Models;
+using Formula1.Models.Contracts;
+using Formula1.Repositories;
+using Formula1.Utilities;
+
+namespace Formula1.Core
+{
+    public class Controller : IController
+    {
+        private PilotRepository pilotRepository;
+        private RaceRepository raceRepository;
+        private FormulaOneCarRepository carRepository;
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            carRepository = new FormulaOneCarRepository();
+        }
+        public string CreatePilot(string fullName)
+        {
+            if (pilotRepository.FindByName(fullName) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotExistErrorMessage,fullName));
+            }
+
+            Pilot pilot = new Pilot(fullName);
+            pilotRepository.Add(pilot);
+            return string.Format(OutputMessages.SuccessfullyCreatePilot, fullName);
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            if (carRepository.FindByName(model) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.CarExistErrorMessage, model));
+            }
+            FormulaOneCar car;
+            if (type == "Ferrari")
+            {
+                 car = new Ferrari(model, horsepower, engineDisplacement);
+            }
+            else if (type == "Williams")
+            {
+                car = new Williams(model, horsepower, engineDisplacement);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidTypeCar, type));
+            }
+            carRepository.Add(car);
+            return string.Format(OutputMessages.SuccessfullyCreateCar, type, model);
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            if (raceRepository.FindByName(raceName) != null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.RaceExistErrorMessage,raceName));
+            }
+
+            Race race = new Race(raceName, numberOfLaps);
+            raceRepository.Add(race);
+            return string.Format(OutputMessages.SuccessfullyCreateRace, raceName);
+        }
+
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            IPilot pilot = pilotRepository.FindByName(pilotName);
+            if (!(pilot is {Car: null}))
+            {
+                throw new InvalidOperationException(
+                    string.Format(ExceptionMessages.PilotDoesNotExistOrHasCarErrorMessage, pilotName));
+            }
+
+            IFormulaOneCar car = carRepository.FindByName(carModel);
+            if (car == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.CarDoesNotExistErrorMessage,
+                    carModel));
+            }
+            pilot.AddCar(car);
+            carRepository.Remove(car);
+            return string.Format(OutputMessages.SuccessfullyPilotToCar, pilotName, car.GetType().Name, carModel);
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+            if (race == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.RaceDoesNotExistErrorMessage,raceName));
+            }
+
+            IPilot pilot = pilotRepository.FindByName(pilotFullName);
+            if (!(pilot is {CanRace: true}) || race.Pilots.Any(x => x.FullName == pilotFullName))
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.PilotDoesNotExistErrorMessage,pilotFullName));
+            }
+            race.Pilots.Add(pilot);
+            return string.Format(OutputMessages.SuccessfullyAddPilotToRace, pilotFullName, raceName);
+        }
+
+        public string StartRace(string raceName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+            if (race == null)
+            {
+                throw new NullReferenceException(
+                    string.Format(ExceptionMessages.RaceDoesNotExistErrorMessage, raceName));
+            }
+
+            if (race.Pilots.Count < 3)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidRaceParticipants, raceName));
+            }
+
+            if (race.TookPlace)
+            {
+                throw new InvalidOperationException(
+                    string.Format(ExceptionMessages.RaceTookPlaceErrorMessage, raceName));
+            }
+
+            IPilot[] pilots = race.Pilots.OrderByDescending(x => x.Car.RaceScoreCalculator(race.NumberOfLaps))
+                .Take(3).ToArray();
+            race.TookPlace = true;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format(OutputMessages.PilotFirstPlace,pilots[0],raceName));
+            pilots[0].WinRace();
+            sb.AppendLine(string.Format(OutputMessages.PilotSecondPlace, pilots[1], raceName));
+            sb.AppendLine(string.Format(OutputMessages.PilotThirdPlace, pilots[2], raceName));
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RaceReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var race in raceRepository.Models.Where(x => x.TookPlace))
+            {
+                sb.AppendLine(race.RaceInfo());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string PilotReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var pilot in pilotRepository.Models.OrderByDescending(x => x.NumberOfWins))
+            {
+                sb.AppendLine(pilot.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 09 April 2022/SmartphoneShop/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SmartphoneShop.Tests
+{
+    [TestFixture]
+    public class SmartphoneShopTests
+    {
+        [Test]
+        public void TestSmartPfoneConstructor()
+        {
+            Smartphone phone = new Smartphone("Samsung", 100);
+            Assert.AreEqual("Samsung",phone.ModelName);
+            phone.ModelName = "Toshiba";
+            Assert.AreEqual("Toshiba",phone.ModelName);
+            phone.CurrentBateryCharge -= 10;
+            Assert.AreEqual(90,phone.CurrentBateryCharge);
+            Assert.AreEqual(100,phone.MaximumBatteryCharge);
+        }
+
+        [Test]
+        public void CheckCreateShopMethod()
+        {
+            Shop shop = new Shop(5);
+
+            Assert.AreEqual(0,shop.Count);
+            Assert.AreEqual(5,shop.Capacity);
+        }
+
+        [Test]
+        public void CheckCreateShopWithInvalidCapacity()
+        {
+            Assert.Throws<ArgumentException>(() => new Shop(-1));
+        }
+
+        [Test]
+        public void CheckCreateShopWithInvalidCapacityMessage()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new Shop(-1));
+            Assert.AreEqual("Invalid capacity.", ex.Message);
+        }
+
+        [Test]
+        public void CheckCreateMethod()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            Assert.AreEqual(1,shop.Count);
+        }
+
+        [Test]
+        public void CheckAddMethodWithExistingPhone()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Toshiba", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            Assert.Throws<InvalidOperationException>(() => shop.Add(phone2));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.Add(phone2));
+            Assert.AreEqual("The phone model Toshiba already exist.",ex.Message);
+        }
+
+        [Test]
+        public void CheckAddMethodWithNoCapacity()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(1);
+            shop.Add(phone);
+            Assert.Throws<InvalidOperationException>(() => shop.Add(phone2));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.Add(phone2));
+            Assert.AreEqual("The shop is full.",ex.Message);
+        }
+
+        [Test]
+        public void CheckRemoveMethodWithValidData()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            Assert.AreEqual(2,shop.Count);
+            shop.Remove("Nexia");
+            Assert.AreEqual(1 , shop.Count);
+        }
+
+        [Test]
+        public void CheckRemoveMethodWithInvalidData()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            Assert.Throws<InvalidOperationException>(() => shop.Remove("Lg"));
+            Assert.Throws<InvalidOperationException>(() => shop.Remove(null));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.Remove("Lg"));
+            Assert.AreEqual("The phone model Lg doesn't exist.",ex.Message);
+        }
+
+        [Test]
+        public void CheckTestPhoneMethodWithValidData()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            shop.TestPhone("Nexia",50);
+            Assert.AreEqual(50,phone2.CurrentBateryCharge);
+        }
+
+        [Test]
+        public void CheckTestPhoneMethodWithNullPhone()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            Assert.Throws<InvalidOperationException>(() => shop.TestPhone("Lg", 20));
+            Assert.Throws<InvalidOperationException>(() => shop.TestPhone(null, 20));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.TestPhone("Lg", 20));
+            Assert.AreEqual("The phone model Lg doesn't exist.",ex.Message);
+        }
+
+        [Test]
+        public void CheckTestMethodWithInvalidBatteryStatus()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 10);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            Assert.Throws<InvalidOperationException>(() => shop.TestPhone("Nexia", 20));
+            Assert.Throws<InvalidOperationException>(() => shop.TestPhone(null, 20));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.TestPhone("Nexia", 20));
+            Assert.AreEqual("The phone model Nexia is low on batery.", ex.Message);
+        }
+
+        [Test]
+        public void CheckChargePhoneWithValidData()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            shop.TestPhone("Nexia", 50);
+            Assert.AreEqual(50,phone2.CurrentBateryCharge);
+            shop.ChargePhone("Nexia");
+            Assert.AreEqual(100,phone2.CurrentBateryCharge);
+        }
+
+        [Test]
+        public void CheckChargeMethodWithInvalidData()
+        {
+            Smartphone phone = new Smartphone("Toshiba", 100);
+            Smartphone phone2 = new Smartphone("Nexia", 100);
+            Shop shop = new Shop(2);
+            shop.Add(phone);
+            shop.Add(phone2);
+            Assert.Throws<InvalidOperationException>(() => shop.ChargePhone(null));
+            Assert.Throws<InvalidOperationException>(() => shop.ChargePhone("Lg"));
+            var ex = Assert.Throws<InvalidOperationException>(() => shop.ChargePhone("Lg"));
+            Assert.AreEqual("The phone model Lg doesn't exist.",ex.Message);
+        }
+    }
+}
